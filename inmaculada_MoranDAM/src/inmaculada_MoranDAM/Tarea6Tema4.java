ba79cbbc8003159cc539efe66ac77b832c28cca4
imoranr02 <imoranr02@iesalbarregas.es>
2021-03-18 00:22:15 +0100

    "Validamos la clase principal, 1º commit"

---
+++ inmaculada_MoranDAM/src/inmaculada_MoranDAM/Tarea6Tema4.java
@@ -0,0 +1,9 @@
+package inmaculada_MoranDAM;
+
+public class Tarea6Tema4 {
+	public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	System.out.println("Hola Inmaculada");
+	}
+	
+}
