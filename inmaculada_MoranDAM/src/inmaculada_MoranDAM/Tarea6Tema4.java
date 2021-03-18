e76f2c017961788c81bc1e25e3308682a064003e
imoranr02 <imoranr02@iesalbarregas.es>
2021-03-18 01:33:10 +0100

    "Creación de rama nueva, 4º commit"

--- inmaculada_MoranDAM/src/inmaculada_MoranDAM/Tarea6Tema4.java
+++ inmaculada_MoranDAM/src/inmaculada_MoranDAM/Tarea6Tema4.java
@@ -4,5 +4,7 @@ public class Tarea6Tema4 {
 	public static void main(String[] args) {
 	// TODO Auto-generated method stub
 	System.out.println("Hola Inmaculada");
+	System.out.println("Hola Inmaculada. Versión 2");
+	System.out.println("Hola Inmaculada. Versión 3");
 	}
 }
